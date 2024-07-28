b95d693b386393c3c2c410c892d1a4df5948ffe0
Jovan <jj200631d@student.etf.bg.ac.rs>
2024-07-28 17:54:37 +0200

    Started working on index page

---
+++ components/Restaurants.tsx
@@ -0,0 +1,73 @@
+import { View, Text, ScrollView, StyleSheet, Image, TouchableOpacity, ImageBackground } from 'react-native';
+import React from 'react';
+import { restaurants } from '@/assets/data/home';
+import { Link } from 'expo-router';
+import {Colors} from '../constants/Colors';
+import { restaurant } from '@/assets/data/restaurant';
+
+const Restaurants = () => {
+  return (
+    <ScrollView
+        paddingHorizontal
+        showHorizontalScrollIndicator={false}
+        contentContainerStyle={{
+            padding: 15,
+        }}
+        >
+        {restaurants.map((restaurant, index)=>(
+            <Link href={'/details'} key={index} asChild>
+                <TouchableOpacity>
+                    <View style={styles.categoryCard}>
+                        <ImageBackground source={restaurant.img} style={styles.image}>
+                            <View style={styles.categoryBox}>
+                                <Text style={styles.categoryText}>{restaurant.name}</Text>
+                                <Text style={{ color: Colors.green }}>
+                                    {restaurant.rating} {restaurant.ratings}
+                                </Text>
+                                <Text style={{ color: Colors.medium }}>{restaurant.distance}</Text>
+                            </View>
+                        </ImageBackground>
+                    </View>
+                </TouchableOpacity>
+            
+            </Link>
+        ))}
+
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+    categoryCard: {
+      width: 300,
+      height: 250,
+      backgroundColor: '#fff',
+      marginEnd: 10,
+      elevation: 2,
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 4,
+      },
+      shadowOpacity: 0.06,
+      borderRadius: 4,
+    },
+    categoryText: {
+      paddingVertical: 5,
+      fontSize: 14,
+      fontWeight: 'bold',
+    },
+    image: {
+      flex: 5,
+      width: undefined,
+      height: undefined,
+    },
+    categoryBox: {
+      flex: 2,
+      padding: 10,
+    },
+  });
+  
+
+
+export default Restaurants
