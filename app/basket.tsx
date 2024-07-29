ae62ad8e1a558e2567cc3040d82d19faf5f9bb7c
Jovan <jj200631d@student.etf.bg.ac.rs>
2024-07-29 22:22:59 +0200

    added basket tab

---
+++ app/basket.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Basket = () => {
+  return (
+    <View>
+      <Text>Basket</Text>
+    </View>
+  )
+}
+
+export default Basket
